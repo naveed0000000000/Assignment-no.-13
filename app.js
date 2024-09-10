4dbe668de9fcc3a71083b62d168c3fedca2ae3a7
Shell <naveed.ahmed@sacl.com>
2024-09-10 14:30:36 +0500

    add files and golders

---
+++ app.js
@@ -0,0 +1,167 @@
+//chapter 5
+//qns 1
+
+//  var num1 = 10;
+//  var num2 = 20;
+
+//  var num = num1 + num2;
+
+//  document.write("Sum of 10 and 20 is = " , num);
+    
+
+// //qns 2
+
+// var num1 = 5;
+// var num2 = 10;
+
+// var num = num1 - num2;
+
+// document.write("subtraction of 5 and 10 is = " , num);
+   
+// var num1 = 15;
+// var num2 = 25;
+
+// var num = num1 * num2;
+
+// document.write("Multipliction of 15 and 25 is = " , num);
+
+// var num1 = 20;
+// var num2 = 30;
+    
+// var num = num1 / num2;
+
+// document.write("division of 20 and 30 is = " , num);
+
+// var num1 = 20;
+// var num2 = 30;
+    
+// var num = num1 % num2;
+
+// document.write("modulus of 20 and 30 is = " , num);
+
+
+//qns 3
+
+// var x;
+
+// document.write("Value after variable declaration is " + x);
+
+// x = 5;
+// document.write("<p>Intitial Value " + x + "</p>");
+
+// x++;
+// document.write("<p>Value after increment is " + x++ + "</p>");
+
+// x += 6;
+// document.write("<p>Value after addition is " + x + "</p>");
+
+// x -= 1;
+// document.write("<p>Value after Decrement is " + x + "</p>");
+
+// x %= 3;
+// document.write("<p>The Remainder is  " + x + "</p>");
+
+
+
+//qns no 4
+
+// let movTick = 600;
+// let totaltick = 5;
+// document.write("Total cost to buy 5 tickets to a movie is " , totaltick * movTick," PKR ");
+
+
+
+// //Qns no 5
+
+// document.write('<p>4x1='+4*1+"</p>")
+// document.write('<p>4x2='+4*2+"</p>")
+// document.write('<p>4x3='+4*3+"</p>")
+// document.write('<p>4x4='+4*4+"</p>")
+// document.write('<p>4x5='+4*5+"</p>")
+// document.write('<p>4x6='+4*6+"</p>")
+// document.write('<p>4x7='+4*7+"</p>")
+// document.write('<p>4x8='+4*8+"</p>")
+// document.write('<p>4x9='+4*9+"</p>")
+// document.write("<p>4x10="+4*10+"</p>")
+
+
+
+//Qns no 6
+
+// var F = 77
+
+
+// var C = (F -32)*5/9
+
+// document.write(C)
+
+// var C = 25
+
+// var F = (C*9/5)+32
+
+// document.write(F)
+
+//qns 7
+
+// let item1 = 650;
+// let item2 = 100;
+// let orderdqunat1 = 3;
+// let orderdqunat2 = 7;
+// let shippingchar = 100;
+
+// let totalcost = item1 * orderdqunat1;
+// let totalcos2 = item2 * orderdqunat2;
+// let subtot = totalcost + totalcos2;
+// let output = subtot + shippingchar;
+// document.write("Total cost of your order is " , output);
+
+
+//qns no 8
+
+// let totalmarks = 980;
+// let marksobtained = 804;
+
+// document.write("student percentage is " , marksobtained/totalmarks*100);
+
+
+//qns no 9;
+
+// let usdollar = 10;
+// let saudiriyal = 25;
+// let rateus = 104.80;
+// let ratesr = 28;
+ 
+// let total1 = usdollar * rateus;
+// let total2 = saudiriyal * ratesr;
+
+// let currencyrates = total1 + total2;
+
+// document.write("<h1>Currency in PKR: ", "</h1>");
+// document.write("Total Currency in PKR: ", currencyrates);
+
+//qns no 10;
+
+// let age = 10;
+// age + 5 * 10
+// let result = age / 2;
+
+// document.write("airthmatic equation is " , result);
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+    
+
+    
+ 
+
+
